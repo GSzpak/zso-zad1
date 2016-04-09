e78733d82a0073598f8a1c097e251d84fa0461f4
Grzegorz Szpak <gszpak@student.uw.edu.pl>
2016-04-09 18:25:46 +0200

    Proper (?) memory mapping

--- raise.c
+++ raise.c
@@ -12,7 +12,7 @@
 
 
 #define MAX_CHAR_BUF_SIZE 80
-#define MAX_NT_FILE_ENTRIES_NUM 100
+#define MAX_NT_FILE_ENTRIES_NUM 1000
 #define STACK_TOP_ADDRESS 0x8000000
 #define INITIAL_STACK_SIZE_IN_PAGES 33
 
@@ -42,7 +42,7 @@ typedef struct {
 
 typedef struct {
     nt_file_entry_header_t header;
-    char file_name[80];
+    char file_path[MAX_CHAR_BUF_SIZE];
 } nt_file_entry_t;
 
 typedef struct {
@@ -68,7 +68,7 @@ void exit_with_error(const char *reason)
 
 void read_and_check_elf_header(int core_file_descriptor, Elf32_Ehdr *elf_header)
 {
-    print("Checking file\n");
+    //print("Checking file\n");
     if (read(core_file_descriptor, elf_header, sizeof(Elf32_Ehdr)) == -1) {
         exit_with_error("Error while reading ELF header\n");
     }
@@ -84,7 +84,7 @@ void read_and_check_elf_header(int core_file_descriptor, Elf32_Ehdr *elf_header)
     if (elf_header->e_type != ET_CORE) {
         exit_with_error("Error: not a CORE file\n");
     }
-    print("Core file OK!\n");
+    //print("Core file OK!\n");
 }
 
 int open_core_file(char *file_path)
@@ -134,36 +134,13 @@ size_t read_file_name(int file_descriptor, char *buffer)
     return current_char_pos;
 }
 
-/*
-void map_file(int core_file_descriptor, off_t *current_offset,
-               nt_file_header_t *nt_file_header, nt_file_entry_t *nt_file_entry)
-{
-    char file_name[MAX_CHAR_BUF_SIZE];
-
-    int file_descriptor = open(file_name, O_RDONLY);
-    if (file_descriptor == -1) {
-        exit_with_error("Error while opening file\n");
-    }
-    // TODO: make checked_ versions of stdlib functions
-    // TODO: set flags properly
-    size_t mapped_area_size = nt_file_entry->end - nt_file_entry->start;
-    off_t offset = nt_file_entry->file_offset * nt_file_header->page_size;
-    void *mapped_area = mmap((void *) nt_file_entry->start, mapped_area_size,
-                             PROT_READ | PROT_WRITE | PROT_EXEC,
-                             MAP_FIXED | MAP_PRIVATE, file_descriptor, offset);
-    if (mapped_area == MAP_FAILED) {
-        exit_with_error("Error in mmap\n");
-    }
-    close(file_descriptor);
-}
-*/
-
 void read_nt_file_section_entries(int core_file_descriptor,
                                   off_t *current_offset,
                                   nt_file_header_t *nt_file_header,
                                   nt_file_entry_t *nt_file_entries)
 {
     int num_of_entries = nt_file_header->number_of_entries;
+    //printf("num of entries: %d\n", num_of_entries);
     for (int i = 0; i < num_of_entries; ++i) {
         nt_file_entry_t *current_entry = nt_file_entries + i;
         int read_result = read(core_file_descriptor,
@@ -172,14 +149,11 @@ void read_nt_file_section_entries(int core_file_descriptor,
         if (read_result == -1) {
             exit_with_error("Error while reading NT_FILE entry\n");
         }
-        // TODO: remove
-        printf("Start: %p, end: %p\n", (void *) (nt_file_entries + i)->header.start,
-               (void *) (nt_file_entries + i)->header.end);
         *current_offset += sizeof(nt_file_entry_header_t);
     }
     for (int i = 0; i < num_of_entries; ++i) {
         size_t name_length = read_file_name(core_file_descriptor,
-                                            (nt_file_entries + i)->file_name);
+                                            (nt_file_entries + i)->file_path);
         // Add one for trailing '\0' character
         *current_offset += (name_length + 1);
     }
@@ -220,18 +194,18 @@ void read_note_entry_descriptor(int core_file_descriptor, off_t *current_offset,
     off_t offset_before_reading_note_section = *current_offset;
     switch (entry_header->type) {
         case NT_FILE:
-            print("NT_FILE found\n");
+            //printf("NT_FILE size: %p\n", entry_header->desc_size);
             read_nt_file_section(core_file_descriptor, current_offset,
                                  &pt_note_info->nt_file_info);
             break;
         case NT_PRSTATUS:
-            print("NT_PRSTATUS found\n");
+            //print("NT_PRSTATUS found\n");
             read_nt_prstatus_section(core_file_descriptor, current_offset,
                                      pt_note_info);
             break;
         case NT_386_TLS:
             // TODO
-            print("NT_386_TLS found\n");
+            //print("NT_386_TLS found\n");
             pt_note_info->nt_386_tls_found = true;
             lseek(core_file_descriptor, entry_header->desc_size, SEEK_CUR);
             *current_offset += entry_header->desc_size;
@@ -275,7 +249,7 @@ void read_note_entry(int core_file_descriptor, off_t *current_offset,
 void read_pt_note_segment(int core_file_descriptor, Elf32_Phdr *program_header,
                           pt_note_info_t *pt_note_info)
 {
-    print("Reading note segment\n");
+    //print("Reading note segment\n");
     lseek(core_file_descriptor, program_header->p_offset, SEEK_SET);
     off_t current_offset = 0;
     size_t note_section_size = program_header->p_filesz;
@@ -284,26 +258,74 @@ void read_pt_note_segment(int core_file_descriptor, Elf32_Phdr *program_header,
     }
 }
 
-void read_pt_load_segment(int core_file_descriptor, Elf32_Phdr *program_header)
+// TODO: files not entirely in PT_LOAD?
+void map_files_in_interval(nt_file_info_t *nt_file_info,
+                           Elf32_Phdr *pt_load_header)
 {
-    print("Reading load segment\n");
-    if (program_header->p_filesz == 0) {
-        // Read-only mapped file - read from PT_NOTE section
-        return;
+    size_t page_size = nt_file_info->header.page_size;
+    int number_of_files = nt_file_info->header.number_of_entries;
+    nt_file_entry_t *file_entries = nt_file_info->entries;
+    addr_t start_addr = (addr_t) pt_load_header->p_vaddr;
+    addr_t end_addr = start_addr + pt_load_header->p_memsz;
+    //printf("%p, %p\n", start_addr, end_addr);
+    for (int i = 0; i < number_of_files; ++i) {
+        nt_file_entry_header_t *current_file_info = &((file_entries + i)->header);
+        if (current_file_info->start >= start_addr &&
+                current_file_info->start < end_addr) {
+            //printf("\t%p, %p\n", current_file_info->start, current_file_info->end);
+            puts((file_entries + i)->file_path);
+            assert(current_file_info->end <= end_addr);
+            int file_descriptor = open((file_entries + i)->file_path, O_RDONLY);
+            if (file_descriptor == -1) {
+                // TODO: support passing many arguments
+                print((file_entries + i)->file_path);
+                exit_with_error("Error while opening file\n");
+            }
+            size_t memory_size = current_file_info->end - current_file_info->start;
+            int protection_flags = pt_load_header->p_flags;
+            int flags = MAP_FIXED | MAP_PRIVATE;
+            off_t file_offset = page_size * current_file_info->file_offset;
+            void *result_addr = mmap((void *) current_file_info->start,
+                                     memory_size, protection_flags, flags,
+                                     file_descriptor, file_offset);
+            if (result_addr == MAP_FAILED) {
+                exit_with_error("Error in mmap\n");
+            }
+            // TODO: remove
+            if (result_addr != (void *) current_file_info->start) {
+                exit_with_error("No kurwa\n");
+            }
+        }
     }
-    void *memory_adress = (void *) program_header->p_vaddr;
-    size_t memory_size = program_header->p_memsz;
+}
+
+void read_pt_load_segment(int core_file_descriptor, Elf32_Phdr *pt_load_header,
+                          nt_file_info_t *nt_file_info)
+{
+    //print("Reading load segment\n");
+    void *memory_adress = (void *) pt_load_header->p_vaddr;
+    size_t memory_size = pt_load_header->p_memsz;
+
+    // TODO: remove
     if (memory_size % getpagesize() != 0) {
         exit_with_error("No kurwa...\n");
     }
-    int prot = program_header->p_flags;
+
     int flags = MAP_FIXED | MAP_ANONYMOUS | MAP_PRIVATE;
-    void *allocated_memory = mmap(memory_adress, memory_size, prot, flags, -1, 0);
+    void *allocated_memory = mmap(memory_adress, memory_size,
+                                  pt_load_header->p_flags, flags,
+                                  -1, 0);
     //printf("Requested: %p, received: %p\n", memory_adress, allocated_memory);
     if (allocated_memory == MAP_FAILED) {
         exit_with_error("Error in mmap\n");
     }
-    lseek(core_file_descriptor, program_header->p_offset, SEEK_SET);
+    map_files_in_interval(nt_file_info, pt_load_header);
+    if (pt_load_header->p_filesz == 0) {
+        // Read-only mapped file - already read from PT_NOTE section
+        return;
+    }
+    // TODO: check lseek and read
+    lseek(core_file_descriptor, pt_load_header->p_offset, SEEK_SET);
     read(core_file_descriptor, allocated_memory, memory_size);
 }
 
@@ -341,7 +363,8 @@ void read_core_file(char *file_path)
                 if (!pt_note_successfully_read) {
                     exit_with_error("PT_LOAD occured before PT_NOTE\n");
                 }
-                read_pt_load_segment(core_file_descriptor_2, &program_header);
+                read_pt_load_segment(core_file_descriptor_2, &program_header,
+                                     &pt_note_info.nt_file_info);
                 break;
             default:
                 break;
@@ -374,6 +397,6 @@ int main(int argc, char *argv[])
 
     read_core_file(argv[1]);
 
-    //print("OK!\n");
+    print("OK!\n");
     return 0;
 }
